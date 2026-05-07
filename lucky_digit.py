3650a574a98125aa4b2c99fd68b660b7fc0da1f0
Sanjana-N-hola <sanjo8827@gmail.com>
2026-05-07 18:28:51 +0530

    We completed two programs myrange and lucky digit

---
+++ lucky_digit.py
@@ -0,0 +1,29 @@
+'''algorithm:
+    find digits of given number until n is zero
+    if
+        if sum is not yet a single digit number then repeat step one
+    else
+        print the sum as output
+    input_num= 789
+    input_num= 789//10 #78
+    remainder=789%10 #9 
+
+'''
+
+input_num=int(input('Enter a number to find your lucky digit:'))
+print(f'Number you input is {input_num}')
+def adder(input_num):
+    sum_of_digits=0
+    remainder=input_num%10
+    input_num=input_num//10
+    sum_of_digits+=remainder
+    if sum_of_digits>9 and input_num==0:
+        return adder(sum_of_digits)
+    else:
+        return sum_of_digits
+            
+
+print(f'Lucky number is',adder(input_num))
+
+
+
---
+++ my_range.py
@@ -0,0 +1,27 @@
+def my_range(*args):
+    i=0
+    if len(args)==1:
+        while i<args[0]:
+            yield i
+            i+=1
+        
+    elif len(args)==2:
+        i=args[0]
+        while i<args[1]:
+            yield i
+            i+=1
+    elif len(args)==3 and args[2]>0:
+        i=args[0]
+        while i<args[1]:
+            yield i
+            i+=args[2]
+
+    elif len(args)==3 and args[2]<0:
+        i=args[0]
+        while i>args[1]:
+            yield i
+            i+=args[2]
+    else:
+        raise TypeError
+for i in my_range(10):
+    print(i)
